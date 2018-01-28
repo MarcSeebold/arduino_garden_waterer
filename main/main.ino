fd04cadfcc6a25b35e10532b475fb488c506f601
Marc Seebold <marc@seebold.eu>
2018-01-28 15:54:29 -0800

    Major refactoring: Removed relay and battery awake stuff. New version will use a water pump and 110V/12V power supply.

--- main/main.ino
+++ main/main.ino
@@ -4,12 +4,6 @@
  * 
  */
 
-
-
-// Timer Interrupts
-#include <StaticThreadController.h>
-#include <Thread.h>
-#include <ThreadController.h>
 // External gClock
 #include <DS3231.h>
 
@@ -17,121 +11,51 @@
 // ----------------------------------------- Globals ------------------------------------------
 // --------------------------------------------------------------------------------------------
 #define DEBUG 1
-int relay = 13;
-int battery_keep_awake1 = 7;
-int battery_keep_awake2 = 8;
-Thread gBatteryAwakeTimer1;
-Thread gBatteryAwakeTimer2;
 // Init the DS3231 using the hardware interface
-// PIN 4 -> SDA (D)
-// PIN 5 -> SCL (C)
+// PIN A4 -> SDA (D)
+// PIN A5 -> SCL (C)
 DS3231 gClock;
-boolean gPM, gh12;
+boolean gh12, gPM;
 // --------------------------------------------------------------------------------------------
 // ------------------------------------------ Setup -------------------------------------------
 // --------------------------------------------------------------------------------------------
 void setup() {
   // put your setup code here, to run once:
   setup_pins();
-  //setup_rtc();
-  setup_battery_awake();
 #ifdef DEBUG
   Serial.begin(115200);
+  Serial.println("Debug enabled.");
 #endif
-}
-
-void setup_battery_awake()
-{
-  // Module 1
-  gBatteryAwakeTimer1 = Thread();
-  gBatteryAwakeTimer1.setInterval(3000);
-  gBatteryAwakeTimer1.enabled = true;
-  gBatteryAwakeTimer1.onRun(keep_battery_awake1);
-  // Module 2
-  gBatteryAwakeTimer2 = Thread();
-  gBatteryAwakeTimer2.setInterval(3000);
-  gBatteryAwakeTimer2.enabled = true;
-  gBatteryAwakeTimer2._cached_next_run = 1500;
-  gBatteryAwakeTimer2.onRun(keep_battery_awake2);
 }
 
 void setup_pins()
 {
-  pinMode(relay, OUTPUT);
-  pinMode(battery_keep_awake1, OUTPUT);
-  pinMode(battery_keep_awake2, OUTPUT);
-  digitalWrite(battery_keep_awake1, LOW);
-  digitalWrite(battery_keep_awake2, LOW);
-  switch_relay_state(false);
+  Serial.println("Setting up pins");
+  Serial.println("Done: Setting up pins");
 }
 
 void setup_rtc()
 {
-  Serial.println(".");
+  Serial.println("Setting up rtc");
   Wire.begin(); // Initialize I2C
-  Serial.println("a");
   gClock.setClockMode(false); // set to 24h
-  Serial.println("b");
   // Set default time (we don't care about the date)
   gClock.setHour(03);
-  
   gClock.setMinute(04);
   gClock.setSecond(05);
-  Serial.println("c");
+  Serial.println("Done: Setting up rtc");
 }
 // --------------------------------------------------------------------------------------------
 // ---------------------------------------- Main logic ----------------------------------------
 // --------------------------------------------------------------------------------------------
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(10);
-  //Serial.println(gClock.getHour(gh12, gPM), DEC);
-  //Serial.println(gClock.getMinute(), DEC);
-  //Serial.println(gClock.getSecond(), DEC);
-  //Serial.println("----------------");
-
-  // First check if our Thread "should" be runned
-  if(gBatteryAwakeTimer1.shouldRun()){
-  // Yes, the Thread should be runned, let's run it
-    gBatteryAwakeTimer1.run();
-  }
-  // First check if our Thread "should" be runned
-  if(gBatteryAwakeTimer2.shouldRun()){
-  // Yes, the Thread should be runned, let's run it
-    gBatteryAwakeTimer2.run();
-  }
-  //delay(5000);
-  //switch_relay_state(true);
-  //delay(2000);
-  //switch_relay_state(false);
-}
-
-void keep_battery_awake1()
-{
-  Serial.println("ping1");
-  digitalWrite(battery_keep_awake1, HIGH);
-  delay(300);
-  digitalWrite(battery_keep_awake1, LOW);
-}
-
-void keep_battery_awake2()
-{
-  Serial.println("ping2");
-  digitalWrite(battery_keep_awake2, HIGH);
-  delay(500);
-  digitalWrite(battery_keep_awake2, LOW);
-}
-
-void switch_relay_state(boolean _on)
-{
-  if (_on)
-  {
-    digitalWrite(relay, LOW);
-  }
-  else
-  {
-    digitalWrite(relay, HIGH);
-  }
+  delay(1000);
+  Serial.println("----------------");
+  Serial.println(gClock.getHour(gh12, gPM), DEC);
+  Serial.println(gClock.getMinute(), DEC);
+  Serial.println(gClock.getSecond(), DEC);
+  Serial.println("----------------");
 }
 
 
