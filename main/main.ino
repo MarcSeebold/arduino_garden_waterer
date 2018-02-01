7e7d18e09242e9b335d988a61008789dfdfc3d0a
Marc Seebold <marc@seebold.eu>
2018-01-31 22:53:49 -0800

    Interrupts for 2 buttons

--- main/main.ino
+++ main/main.ino
@@ -12,7 +12,7 @@
 // --------------------------------------------------------------------------------------------
 // ----------------------------------------- Globals ------------------------------------------
 // --------------------------------------------------------------------------------------------
-#define DEBUG 0
+#define DEBUG 1
 // Init the DS3231 using the hardware interface
 // PIN A4 -> SDA (D)
 // PIN A5 -> SCL (C)
@@ -20,7 +20,13 @@ DS3231 gClock;
 boolean gh12, gPM;
 
 // PINS
-int pump = 7;
+int pin_pump = 7;
+int pin_button_green = 3;
+int pin_button_red = 2;
+
+// Buttons
+volatile int button_red_was_pressed = false;
+volatile int button_green_was_pressed = false;
 
 // LCD display
 LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line
@@ -51,14 +57,28 @@ void setup_lcd()
 
 void setup_pins()
 {
-  pinMode(pump, OUTPUT);
-  digitalWrite(pump, HIGH); // HIGH = off!
+  pinMode(pin_pump, OUTPUT);
+  digitalWrite(pin_pump, HIGH); // HIGH = off!
+  pinMode(pin_button_red, INPUT_PULLUP);
+  pinMode(pin_button_green, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pin_button_red), button_red_pressed, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pin_button_green), button_green_pressed, CHANGE);
+}
+
+void button_red_pressed()
+{
+  button_red_was_pressed = true;
+}
+
+void button_green_pressed()
+{
+  button_green_was_pressed = true;
 }
 
 // false = off, true = on
 void set_pump(boolean state = false)
 {
-  digitalWrite(pump, state ? LOW : HIGH); // HIGH = off!
+  digitalWrite(pin_pump, state ? LOW : HIGH); // HIGH = off!
 }
 
 void setup_rtc()
@@ -66,9 +86,9 @@ void setup_rtc()
   Serial.println("Setting up rtc");
   gClock.setClockMode(false); // set to 24h
   // Set default time (we don't care about the date)
-  gClock.setHour(11);
-  gClock.setMinute(12);
-  gClock.setSecond(13);
+  gClock.setHour(10);
+  gClock.setMinute(21);
+  gClock.setSecond(50);
   Serial.println("Done: Setting up rtc");
 }
 
@@ -103,7 +123,22 @@ void loop() {
     print_time();
   else
     print_temperature();
-  delay(5000);
+
+  if (button_red_was_pressed)
+  {
+    button_red_was_pressed = false;
+    lcd.clear();
+    lcd.print("red!");
+  }
+  if (button_green_was_pressed)
+  {
+    button_green_was_pressed = false;
+    lcd.clear();
+    lcd.print("green!");    
+  }
+
+  delay(2000);
+  Serial.println("ping");
 }
 
 
