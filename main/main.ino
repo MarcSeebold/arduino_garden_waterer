c5aa84944827109ce581d43fb8517b59612f8491
Marc Seebold <marc@seebold.eu>
2018-01-31 23:21:51 -0800

    Menu for setting the time

--- main/main.ino
+++ main/main.ino
@@ -61,8 +61,8 @@ void setup_pins()
   digitalWrite(pin_pump, HIGH); // HIGH = off!
   pinMode(pin_button_red, INPUT_PULLUP);
   pinMode(pin_button_green, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pin_button_red), button_red_pressed, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(pin_button_green), button_green_pressed, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pin_button_red), button_red_pressed, RISING);
+  attachInterrupt(digitalPinToInterrupt(pin_button_green), button_green_pressed, RISING);
 }
 
 void button_red_pressed()
@@ -75,21 +75,104 @@ void button_green_pressed()
   button_green_was_pressed = true;
 }
 
-// false = off, true = on
-void set_pump(boolean state = false)
-{
-  digitalWrite(pin_pump, state ? LOW : HIGH); // HIGH = off!
-}
-
 void setup_rtc()
 {
   Serial.println("Setting up rtc");
   gClock.setClockMode(false); // set to 24h
   // Set default time (we don't care about the date)
-  gClock.setHour(10);
-  gClock.setMinute(21);
-  gClock.setSecond(50);
+  gClock.setHour(0);
+  gClock.setMinute(0);
+  gClock.setSecond(0);
   Serial.println("Done: Setting up rtc");
+}
+// --------------------------------------------------------------------------------------------
+// ---------------------------------------- Main logic ----------------------------------------
+// --------------------------------------------------------------------------------------------
+boolean in_menu = false;
+enum menu_pos_t
+{
+  HOUR,
+  MINUTE,
+  SECOND
+};
+menu_pos_t menu_pos = menu_pos_t::HOUR;
+// -------------------------------------Set time buttons---------------------------------------
+void draw_menu()
+{
+  // Draw time
+  lcd.clear();
+  lcd.setCursor(0,0);
+  int hh = gClock.getHour(gh12, gPM);
+  int mm = gClock.getMinute();
+  int ss = gClock.getSecond();
+  lcd.print(String(hh) + ":" + String(mm) + ":" + String(ss));
+  // Draw "cursor"
+  switch(menu_pos)
+  {
+    case menu_pos_t::HOUR:    lcd.setCursor(0,1); break;
+    case menu_pos_t::MINUTE:  lcd.setCursor(3,1); break;
+    case menu_pos_t::SECOND:  lcd.setCursor(6,1); break;
+  }
+  lcd.print("^^");
+}
+
+void time_increase_hour()
+{
+  int old = gClock.getHour(gh12, gPM);
+  gClock.setHour((old+1)%24);
+}
+
+void time_increase_minute()
+{
+  int old = gClock.getMinute();
+  gClock.setMinute((old+1)%60);
+}
+
+void time_increase_second()
+{
+  int old = gClock.getSecond();
+  gClock.setSecond((old+1)%60);
+}
+
+void handle_buttons()
+{
+  if (button_red_was_pressed || button_green_was_pressed)
+  {
+    // Open menu (any button)
+    if (!in_menu)
+    {
+      in_menu = true;
+    }
+    // red button: Select next field
+    else if (button_red_was_pressed)
+    {
+      switch(menu_pos)
+      {
+        case menu_pos_t::HOUR:    menu_pos = menu_pos_t::MINUTE;                   break;
+        case menu_pos_t::MINUTE:  menu_pos = menu_pos_t::SECOND;                   break;
+        case menu_pos_t::SECOND:  menu_pos = menu_pos_t::HOUR;    in_menu = false; break;
+      }
+    }
+    // green button: Increase field
+    else if (button_green_was_pressed)
+    {
+      switch(menu_pos)
+      {
+        case menu_pos_t::HOUR:    time_increase_hour();   break;
+        case menu_pos_t::MINUTE:  time_increase_minute(); break;
+        case menu_pos_t::SECOND:  time_increase_second(); break;
+      }
+    }
+  }
+  button_red_was_pressed = false;
+  button_green_was_pressed = false;
+}
+// --------------------------------------------------------------------------------------------
+
+// false = off, true = on
+void set_pump(boolean state = false)
+{
+  digitalWrite(pin_pump, state ? LOW : HIGH); // HIGH = off!
 }
 
 void print_temperature()
@@ -112,10 +195,16 @@ void print_time()
   lcd.setCursor(0,1);
   lcd.print(String(hh) + ":" + String(mm) + ":" + String(ss));
 }
-// --------------------------------------------------------------------------------------------
-// ---------------------------------------- Main logic ----------------------------------------
-// --------------------------------------------------------------------------------------------
+
+// MAIN LOOP
 void loop() {
+  handle_buttons();
+  if (in_menu)
+  {
+    draw_menu();
+    delay(200);
+    return;
+  }
   // put your main code here, to run repeatedly:
   static bool a = false;
   a = !a;
@@ -123,19 +212,6 @@ void loop() {
     print_time();
   else
     print_temperature();
-
-  if (button_red_was_pressed)
-  {
-    button_red_was_pressed = false;
-    lcd.clear();
-    lcd.print("red!");
-  }
-  if (button_green_was_pressed)
-  {
-    button_green_was_pressed = false;
-    lcd.clear();
-    lcd.print("green!");    
-  }
 
   delay(2000);
   Serial.println("ping");
